82349bcf2a101ecbb69bd269feb9143ca7e7c9ac
Jeremy Yang <jeremyjyang@gmail.com>
2025-12-10 13:03:25 -0700

    Ok.

--- sql/table_duplicates_find.sql
+++ sql/table_duplicates_find.sql
@@ -1,12 +1,10 @@
 -- Find table duplicates.
 SELECT
-        u.cpt_id,
-        u.name,
-        u.provenance
+        *
 FROM
         umls_cpt u
 WHERE
         u.cpt_id IN (SELECT cpt_id FROM umls_cpt GROUP BY cpt_id HAVING COUNT(cpt_id) > 1)
 ORDER BY
-        u.cpt_id, provenance
+        u.cpt_id
         ;
